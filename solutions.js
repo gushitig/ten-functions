b09896cb8fbcf36f77f3f2b3b28ae9b454f4533c
Christina Antonuccio <antonuccio.christina@gmail.com>
2022-11-29 15:30:25 -0600

    first

--- solutions.js
+++ solutions.js
@@ -20,4 +20,94 @@
  * and
  * concat
  */
+// Q1
+function isTrue(input1) {
+    return input1 === true;
+}
+console.log( "Q1 " + isTrue("banana"));
+
+
+//Q2
+function isFalse(input2) {
+    return input2 === false;
+}
+console.log( "Q2 " + isFalse(false));
+
+
+//Q3
+function not(input3) {
+    if (Boolean(input3) !== true) {
+        return true;
+    } else if (Boolean(input3) !== false) {
+        return false;
+    }
+}
+console.log( "Q3 " + not(123));
+
+
+//Q4
+function addOne(input4) {
+    if (parseFloat(input4) !== NaN) {
+        return (parseFloat(input4) + 1);
+    } else {
+        return "NaN";
+    }
+}
+console.log( "Q4 " + addOne("banana"));
+
+
+//Q5
+function isEven(input5) {
+    return parseFloat(input5) % 2 === 0;
+}
+console.log( "Q5 " + isEven(false));
+
+
+//Q6
+function isIdentical(input6a, input6b) {
+    return input6a === input6b;
+}
+console.log( "Q6 " + isIdentical(3, 3.0));
+
+
+
+
+//Q7
+function isEqual(input7a, input7b) {
+    return input7a == input7b;
+}
+console.log( "Q7 " + isEqual(4, -5));
+
+
+
+//Q8
+function or(input8a, input8b) {
+    return (input8a || input8b);
+}
+console.log( "Q8 " + or(true, false));
+
+
+//Q9
+function and(input9a, input9b) {
+    return (input9a && input9b)
+}
+console.log( "Q9 " + and("hello", "world"));
+
+
+
+//Q10
+function concat(input10a, input10b) {
+    if ((typeof input10a == "string" || "number") && (typeof input10b == "string" || "number")) {
+        return String(input10a) + String(input10b);
+    }
+}
+console.log( "Q10 " + concat("abc", 4));
+
+
+
+
+
+
+
+
 
